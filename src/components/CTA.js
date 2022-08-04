7c50f34d50d045b9fb876f9346af2f69c087e5c4
apple <apple@apples-MacBook-Pro.local>
2022-08-04 08:09:47 +0300

    footer done

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -3,20 +3,18 @@ import React from "react"
 const CTA = () => {
   return (
     <section className="w-full mx-auto bg-brightRed " id="cta">
-      <div className="container flex flex-col items-center justify-between px-4 py-24 mx-auto space-y-12 md:py-12 md:flex-row md:space-y-0 md:px-24">
+      <div className="container flex flex-col items-center justify-between px-4 py-24 mx-auto space-y-12 lg:py-12 lg:flex-row md:space-y-0 lg:px-24 ">
         {/*  Heading */}
-        <h2 className="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:max-w-xl md:text-left">
+        <h2 className="text-5xl font-bold leading-tight text-center text-white md:text-4xl md:mb-12 md:max-w-xl lg:text-left lg:mb-0">
           Simplify how your team works today.
         </h2>
         {/*  Button */}
-        <div>
-          <a
-            href="#"
-            className="p-3 px-8 bg-white rounded-full shadow-2xl text-brightRed baseline hover:bg-gray-900"
-          >
-            Get Started
-          </a>
-        </div>
+        <a
+          href="#"
+          className="p-3 px-8 bg-white rounded-full shadow-2xl text-brightRed baseline hover:bg-gray-900"
+        >
+          Get Started
+        </a>
       </div>
     </section>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,35 +4,35 @@ import { Link } from "gatsby"
 const Footer = props => {
   return (
     <div className="w-full mx-auto bg-veryDarkBlue">
-      <div className="container flex flex-col-reverse justify-between px-4 py-10 mx-auto space-y-8 md:flex-row md:space-y-0 md:px-24">
+      <div className="container flex flex-col-reverse justify-between px-4 py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0 lg:px-16">
         {/* Logo and social links container */}
-        <div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
-          <div className="mx-auto my-6 text-center text-white md:hidden">
+        <div className="flex flex-col-reverse items-center justify-between space-y-12 lg:flex-col lg:space-y-0 lg:items-start">
+          <div className="mx-auto my-6 text-center text-white lg:hidden">
             Copyright &copy; 2022, All Rights Reserved
           </div>
           {/*  Logo */}
           <div>
             <Link to="/">
-              <img src="./logo-white.svg" className="h-8" alt="" />
+              <img src="./logo-white.svg" className="h-6" alt="" />
             </Link>
           </div>
           {/* Social Links Container */}
           <div className="flex justify-center space-x-4">
             {/*  Link 1 */}
             <a href="#">
-              <img src="./icon-facebook.svg" alt="" className="h-8" />
+              <img src="./icon-facebook.svg" alt="" className="h-6" />
             </a>
             {/* Link 2 */}
             <a href="#">
-              <img src="./icon-youtube.svg" alt="" className="h-8" />
+              <img src="./icon-youtube.svg" alt="" className="h-6" />
             </a>
             {/*  Link 3  */}
             <a href="#">
-              <img src="./icon-twitter.svg" alt="" className="h-8" />
+              <img src="./icon-twitter.svg" alt="" className="h-6" />
             </a>
             {/*  Link 4 */}
             <a href="#">
-              <img src="./icon-pinterest.svg" alt="" className="h-8" />
+              <img src="./icon-pinterest.svg" alt="" className="h-6" />
             </a>
             {/* Link 5 */}
             <a href="#">
@@ -72,7 +72,7 @@ const Footer = props => {
         {/*  Input Container  */}
         <div className="flex flex-col justify-between">
           <form>
-            <div className="flex space-x-3">
+            <div className="flex max-w-md mx-auto space-x-3">
               <input
                 type="text"
                 className="flex-1 px-4 rounded-full focus:outline-none"
@@ -83,7 +83,7 @@ const Footer = props => {
               </button>
             </div>
           </form>
-          <div className="hidden text-white md:block">
+          <div className="hidden text-white lg:block">
             Copyright &copy; 2022, All Rights Reserved
           </div>
         </div>
